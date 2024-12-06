f5a8e452f311c08869ebda2b8f2cf4d6a8766a32
iLay1678 <bestilay@outlook.com>
2024-12-06 14:29:34 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
-  nitro: {
-    preset: "./cloudflare-preset"
-  },
-
   experimental: {
   },
 
