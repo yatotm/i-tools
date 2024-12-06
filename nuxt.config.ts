afe410b064f902b981802813755c32b04123ee34
iLay1678 <bestilay@outlook.com>
2024-12-06 14:49:10 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  }
   experimental: {
   },
 
