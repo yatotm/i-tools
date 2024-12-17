dc1ba47be6c8e2dce011c17a48aa3226c4af8c1c
我若为王 <ifwangs400@gmail.com>
2024-12-17 15:27:26 +0800

    排序

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+
     <head>
         <title>爱拓工具箱</title>
     </head>
@@ -9,7 +10,8 @@
                     <NuxtLink v-if="tool.available" :to="tool.path" class="block">
                         <a-card hoverable>
                             <template #title>
-                                <h2 class="text-xl font-medium" :class="tool.available ? 'text-gray-800' : 'text-gray-400'">
+                                <h2 class="text-xl font-medium"
+                                    :class="tool.available ? 'text-gray-800' : 'text-gray-400'">
                                     {{ tool.title }}
                                 </h2>
                             </template>
@@ -31,6 +33,12 @@
 <script setup>
 const tools = [
     {
+        id: 999,
+        title: '敬请期待',
+        description: '更多工具正在开发中...',
+        path: '',
+        available: false
+    }, {
         id: 1,
         title: '阿里云盘TV授权',
         description: '获取阿里云盘TV端的授权令牌',
@@ -44,15 +52,8 @@ const tools = [
         path: '/move-car',
         available: true
     },
-    {
-        id: 3,
-        title: '敬请期待',
-        description: '更多工具正在开发中...',
-        path: '',
-        available: false
-    },
-];
+
+].sort((a, b) => a.id - b.id);
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
